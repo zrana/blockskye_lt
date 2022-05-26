91bb9bec233337359985f2175e97bf93a3805527
Ahmed Nawaz <ahmed.nawaz@arbisoft.com>
2022-05-26 17:17:29 +0500

    lt multiple apis

--- locust/BlockSkyeLocustTasks.py
+++ locust/BlockSkyeLocustTasks.py
@@ -17,7 +17,7 @@ class RegistrationTasks(TaskSet):
         Initialize the Task set.
         """
         super(RegistrationTasks, self).__init__(*args, **kwargs)
-        self.register_page = Registration(self.user.host, self.client)
+        self.lt_blockskye = Registration(self.user.host, self.client)
         #self.application_page = ApplicationFlow(self.user.host, self.client)
         #self.login_page = Login(self.user.host, self.client)
         # self.token = ''
@@ -39,13 +39,38 @@ class RegistrationTasks(TaskSet):
     def register_task(self):
         # a = randint(20, 25)
         # time.sleep(a)
+
+        # create profile
         account = 'zeeshan.rana'
         user_email = '{}+{}@arbisoft.com'.format(account, str(uuid.uuid4().node))
         username = 'zrana{}'.format(str(uuid.uuid4().node))
-        #self.register_page.register(user_email, username)
-        response = self.register_page.create_expense()
+        self.lt_blockskye.register(user_email, username)
+
+        # update profile
+        new_user_email = '{}+{}@arbisoft.com'.format(account, str(uuid.uuid4().node))
+        new_username = 'zrana{}'.format(str(uuid.uuid4().node))
+        self.lt_blockskye.update_profile(username, new_user_email, new_username)
+
+        # get profile
+        self.lt_blockskye.get_profile(username)
+
+        # get calendar
+        self.lt_blockskye.get_calendar(username)
+
+        # create expense
+        response = self.lt_blockskye.create_expense()
+
+        # add ticket data
         responseBody = json.loads(response.text)
-        self.register_page.add_ticket_data(responseBody['expense_approval_id'], responseBody['suvtpNumber']['accountNumber'])
+        self.lt_blockskye.add_ticket_data(responseBody['expense_approval_id'], responseBody['suvtpNumber']['accountNumber'])
+        
+        # get expense
+        self.lt_blockskye.get_expense(responseBody['expense_approval_id'])
+
+        # delete profile
+        self.lt_blockskye.delete_profile(username)
+
+        
         # self.application_page.visit_application_page()
         # self.applicaxtion_page.visit_contact_page()
         # self.application_page.fill_contact_form(user_email, username)
